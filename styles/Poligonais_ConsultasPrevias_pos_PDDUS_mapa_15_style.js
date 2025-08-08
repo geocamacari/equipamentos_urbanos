bbafff1d887c3e33c8115d5acaf9d3a7e80ac8ec
geocamacari <fmouracosta@yahoo.com.br>
2025-08-08 14:43:53 -0300

    Add files via upload

---
+++ styles/Poligonais_ConsultasPrevias_pos_PDDUS_mapa_15_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Poligonais_ConsultasPrevias_pos_PDDUS_mapa_15 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Arial Black\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("processo") !== null) {
+        labelText = String(feature.get("processo"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.508}),fill: new ol.style.Fill({color: 'rgba(9,1,255,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
